4fa35eeef481e402647ee018ad3621df9530f095
shalookumari <78845430+shalookumari@users.noreply.github.com>
2021-03-26 14:53:28 +0530

    Restful

---
+++ Restful_Restful/src/main/java/com/ltts/Restful/Service/UserService.java
@@ -0,0 +1,25 @@
+package com.ltts.Restful.Service;
+
+
+import java.util.List; 
+import javax.ws.rs.GET; 
+import javax.ws.rs.Path; 
+import javax.ws.rs.Produces; 
+import javax.ws.rs.core.MediaType;
+
+import com.ltts.Restful.Dao.UserDao;
+import com.ltts.Restful.Model.User;  
+@Path("/UserService") 
+// Specify a path for the web service using @Path annotation to the UserService
+
+public class UserService {  
+   UserDao userDao = new UserDao();  
+   @GET 
+   @Path("/users") 
+   //specify a path for the particular web service method using @Path annotation
+   //to method of UserService.
+   @Produces(MediaType.APPLICATION_XML) 
+   public List<User> getUsers(){ 
+      return userDao.getAllUsers(); 
+   }  
+}
